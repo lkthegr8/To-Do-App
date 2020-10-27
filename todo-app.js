d2cfb30ac43d28c385a6564a56c7cf4dbc05bc74
Lakshmikanth Mhetre <lkthegr8@gmail.com>
2020-10-27 12:56:49 +0530

    video 68 using third party library uuid ( universally unique id )

--- todo-app.js
+++ todo-app.js
@@ -22,6 +22,7 @@ document.querySelector("#search-todos").addEventListener("input",function(e){
 document.querySelector("#new-todo").addEventListener("submit",function(e){
     e.preventDefault()
     todos.push({
+        id : uuidv4(),
         text : e.target.elements.text.value,
         completed : false
     })
